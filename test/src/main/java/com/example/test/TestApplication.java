8d069ccb311ec6f4d15178d74ca22bf58f50e586
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-09-30 01:38:41 +0300

    Fixed RequestParam value in greeting controller

--- test/src/main/java/com/example/test/TestApplication.java
+++ test/src/main/java/com/example/test/TestApplication.java
@@ -3,7 +3,8 @@ package com.example.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //this adds the following annotations
+// @Configuration @EnableAutoConfiguration and @ComponentScan to my application
 public class TestApplication {
 
 	public static void main(String[] args) {
